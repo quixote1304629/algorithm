2e8ab758e325cdf6b2cdfec422c5d13e44e4d7b4
liyang <1032199882@qq.com>
2021-07-19 19:58:58 +0800

    a

--- dy-plan/backPack/index.js
+++ dy-plan/backPack/index.js
@@ -1,13 +1,11 @@
-/** 
+/**
  * 在n个物品中挑选若干物品装入背包，最多能装多满？假设背包的大小为m，每个物品的大小为A[i]
- * 
+ *
  * 输入: [3,4,8,5],  10
  * 输出: 9
  */
 
-
- class Solution {
-
+class Solution {
   /**
    * backPack
    *
@@ -16,7 +14,12 @@
    * @return: The maximum size
    */
   backPack(m, A) {
-    // write your code here
+    const dp = new Array(m + 1).fill(0);
+    for (let i = 0; i < A.length; i++) {
+      for (let j = m; j >= A[i]; j--) {
+        dp[j] = Math.max(dp[j - A[i]] + A[i], dp[j]);
+      }
+    }
+    return dp[m];
   }
-
 }

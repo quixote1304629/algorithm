ec68283187561e64a6a7db2930bd6d11ac5e69ca
liyang <1032199882@qq.com>
2021-09-01 11:00:13 +0800

    a

--- temp.js
+++ temp.js
@@ -1,16 +1,17 @@
-function dfs(str, list = [], result = []) {
-  if (!str) {
-    result.push(list);
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
+      }
+    }
   }
-
-  for (let i = 0; i < 3 && i < str.length; i++) {
-    const a = str.substr(0, i + 1);
-    const b = str.substr(i + 1);
-    const newList = [...list, a];
-    dfs(b, newList, result);
-  }
-  return result;
-}
-
-const res = dfs("12345");
-console.log(res);
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};

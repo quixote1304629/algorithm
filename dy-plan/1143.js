ed85414cc53b4b57205525c896191d955d8e0f70
liyang <1032199882@qq.com>
2021-09-08 17:15:53 +0800

    a

--- dy-plan/1143.js
+++ dy-plan/1143.js
@@ -39,27 +39,28 @@ longestCommonSubsequence('abc', 'def')
 var longestCommonSubsequence = function (text1, text2) {
   const length1 = text1.length;
   const length2 = text2.length;
+  // dp[i] 表示 text1[0,length1-1] 与 text2[0,i-1] 的最长公共子序列
   const dp = Array.from({ length: length2 + 1 }).map(() => 0);
-  for (let i = 1; i <= length1; i++) {
-    const c1 = text1.charAt(i-1) 
-    let prev = dp[0]
+  for (let i = 0; i < length1; i++) {
+    const c1 = text1.charAt(i);
+    let prev = dp[0];
     for (let j = 1; j <= length2; j++) {
-      const temp = dp[j]
-      const c2 = text2.charAt(j-1) 
-      if(c1 === c2) {
-        dp[j] = prev + 1
+      const temp = dp[j];
+      const c2 = text2.charAt(j - 1);
+      if (c1 === c2) {
+        dp[j] = prev + 1;
       } else {
-        dp[j] = Math.max(dp[j-1], dp[j])
+        dp[j] = Math.max(dp[j], dp[j - 1]);
       }
-      prev = temp
+      prev = temp;
     }
   }
-  return dp[length2]
+  return dp[length2];
 };
 
 
 
-const s1 = "dknkdizqxkdczafixidorgfcnkrirmhmzqbcfuvojsxwraxe"
-const s2 = "dulixqfgvipenkfubgtyxujixspoxmhgvahqdmzmlyhajerqz"
+const s1 = "bl"
+const s2 = "yby"
 const res = longestCommonSubsequence(s1, s2)
 console.log(res)
--- temp.js
+++ temp.js
@@ -1,47 +1,27 @@
-const str = "123abd3-adfz-34-akjkfaf";
-
-function isLetter(c = "") {
-  const asc = c.codePointAt();
-  return (asc >= 65 && asc <= 90) || (asc >= 97 && asc <= 122);
-}
-
-/**
- * 翻转不包含字母的字符串
- * 输入: "123abd3-adfz-34-akjkfaf"
- * 输入: "321abd-3adfz-43-akjkfaf"
- * @param {*} str
- */
-function reverseStr(str = "") {
-  let ans = "";
-  let i = 0;
-  let j = 1;
-  // i位置的字母是否是字母
-  let flag = isLetter(str.charAt(i));
-  while (j < str.length) {
-    const isCurrLetter = isLetter(str.charAt(j));
-    if (isCurrLetter === flag) {
-      j++;
-    } else {
-      if (flag) {
-        ans += str.slice(i, j);
+/** dp一维 */
+var longestCommonSubsequence = function (text1, text2) {
+  const length1 = text1.length;
+  const length2 = text2.length;
+  // dp[i] 表示 text1[0,length1-1] 与 text2[0,i-1] 的最长公共子序列
+  const dp = Array.from({ length: length2 + 1 }).map(() => 0);
+  for (let i = 0; i < length1; i++) {
+    const c1 = text1.charAt(i);
+    let prev = dp[0];
+    for (let j = 1; j <= length2; j++) {
+      const temp = dp[j];
+      const c2 = text2.charAt(j - 1);
+      if (c1 === c2) {
+        dp[j] = prev + 1;
       } else {
-        ans += str.slice(i, j).split("").reverse().join("");
+        dp[j] = Math.max(dp[j], dp[j - 1]);
       }
-
-      i = j;
-      flag = isCurrLetter;
-      j++;
+      prev = temp;
     }
   }
+  return dp[length2];
+};
 
-  if (flag) {
-    ans += str.slice(i, j);
-  } else {
-    ans += str.slice(i, j).split("").reverse().join("");
-  }
-
-  return ans;
-}
-
-const result = reverseStr("123abd3-adfz-34-akjkfaf54f");
-console.log(result);
+const s1 = "bl"
+const s2 = "yby"
+const res = longestCommonSubsequence(s1, s2)
+console.log(res)

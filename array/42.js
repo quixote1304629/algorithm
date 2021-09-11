b3836426511f7baefbeb6d1e7db614ebda0b0e49
liyang <1032199882@qq.com>
2021-09-11 09:36:08 +0800

    a

--- array/42.js
+++ array/42.js
@@ -1,3 +1,9 @@
+/**
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ */
+
+
 function calc(list, m, n) {
   if (n - m < 2) return 0;
   const h = Math.min(list[m], list[n]);

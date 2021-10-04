986022a0416498ac686c52865e93f77d7c940311
liyang <1032199882@qq.com>
2021-10-04 12:17:50 +0800

    a

--- str/qianfenwei.js
+++ str/qianfenwei.js
@@ -22,20 +22,53 @@ function qianfenwei(num) {
   return result;
 }
 
+/** 重点 */
+function qianfenwei2(num) {
+  const str = num.toString();
+  const dotIndex = str.indexOf(".");
+  const intLength = dotIndex > 0 ? dotIndex : str.length;
+  // 从整数部分中分割出的字符串数组
+  const arr = [];
+  // 逗号起点
+  const commaStart = intLength % 3;
+
+  if (commaStart > 0) {
+    arr.push(str.slice(0, commaStart));
+  }
+
+  for (let i = commaStart; i < intLength; i += 3) {
+    arr.push(str.slice(i, i + 3));
+  }
+
+  return arr.join(",") + str.slice(intLength);
+}
+
+/** 重点 */
 function numFormat(num) {
   var res = num.toString().replace(/\d+/, function (n) {
-    // 先提取整数部分
+    // 先提取整数部分 (?=x) 含义: 以x结尾
     // return n.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
 
     return n.replace(/(\d)(?=(\d{3})+$)/g, function ($1) {
-      console.log($1)
+      console.log($1);
       return $1 + ",";
     });
   });
   return res;
 }
 
-12345678.2569.toLocaleString()
+function numFormat2(num) {
+  const splitArr = num.toString().split(".");
+  const intPart = splitArr[0];
+  const intFmt = intPart.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+  if(splitArr.length > 1) {
+    return intFmt + '.' + splitArr[1]
+  } else {
+    return intFmt
+  }
+}
 
-const res = numFormat(12345678.2569);
+(12345678.2569).toLocaleString();
+
+const res = numFormat2(12.2569);
 console.log(res);

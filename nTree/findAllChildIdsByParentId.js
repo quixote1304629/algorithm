0afa9d5094d5608aec7e4b828ec064dca9b814ed
liyang <1032199882@qq.com>
2021-10-12 21:48:16 +0800

    a

--- nTree/findAllChildIdsByParentId.js
+++ nTree/findAllChildIdsByParentId.js
@@ -15,19 +15,34 @@ function findNodeById(treeList, id) {
 }
 
 /** 找到给定节点的所用子节点id,返回所有的子节点id数组 */
+// function findAllChildIdsByNode(node) {
+//   const result = [];
+//   if (!node.children || !node.children.length) return result;
+//   const queue = [...node.children];
+//   let i = 0;
+//   while (i < queue.length) {
+//     const curr = queue[i];
+//     result.push(curr.id);
+//     if (curr.children && curr.children.length) {
+//       queue.splice(queue.length, 0, ...curr.children);
+//     }
+//     i++;
+//   }
+//   return result;
+// }
+
+/** 找到给定节点的所用子节点id,返回所有的子节点id数组 */
 function findAllChildIdsByNode(node) {
   const result = [];
-  if (!node.children || !node.children.length) return result;
-  const queue = [...node.children];
-  let i = 0;
-  while (i < queue.length) {
-    const curr = queue[i];
-    result.push(curr.id);
-    if (curr.children && curr.children.length) {
-      queue.splice(queue.length, 0, ...curr.children);
+  function dfs(list) {
+    if (!list || !list.length) return;
+    for (let i = 0; i < list.length; i++) {
+      const curr = list[i];
+      result.push(curr.id);
+      dfs(curr.children);
     }
-    i++;
   }
+  dfs(node.children);
   return result;
 }
 
@@ -48,16 +63,44 @@ const data = [
         id: "11",
         name: "test11",
         children: [
-          { id: "111", name: "test111" },
-          { id: "112", name: "test112" },
+          {
+            id: "111",
+            name: "test111",
+            children: [
+              { id: "1111", name: "test1111" },
+              { id: "1112", name: "test1112" },
+            ],
+          },
+          {
+            id: "112",
+            name: "test112",
+            children: [
+              { id: "1121", name: "test1121" },
+              { id: "1122", name: "test1122" },
+            ],
+          },
         ],
       },
       {
         id: "12",
         name: "test12",
         children: [
-          { id: "121", name: "test121" },
-          { id: "122", name: "test122" },
+          {
+            id: "121",
+            name: "test121",
+            children: [
+              { id: "1211", name: "test1211" },
+              { id: "1212", name: "test1212" },
+            ],
+          },
+          {
+            id: "122",
+            name: "test122",
+            children: [
+              { id: "1221", name: "1221" },
+              { id: "1222", name: "1222" },
+            ],
+          },
         ],
       },
     ],
@@ -87,5 +130,5 @@ const data = [
   },
 ];
 
-const res = findAllChildIdsByParentId(data, "1");
+const res = findAllChildIdsByParentId(data, "12");
 console.log(res);
